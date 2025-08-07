8a1e710e454ba7d62054fc726070b2d6a7b81b1b
Tombstone119 <kodagodayohan@gmail.com>
2025-08-07 20:38:59 +0530

    feat: added header and footer nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
+import Header from "@/components/layouts/header";
+import Footer from "@/components/layouts/footer";
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +32,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <>
+          <Header />
+          <main className="w-full">{children}</main>
+          <Footer />
+        </>
       </body>
     </html>
   );
